45ccd63c0704cf6398284c500a1c4de96a23ecf9
rajarya199 <aryalrajan078@gmail.com>
2025-07-12 13:58:26 +0545

    social log

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { signIn } from "next-auth/react";
 import { FaGoogle, FaGithub, FaLinkedin } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
 
 type ProviderId = "google" | "github" | "linkedin";
 
@@ -16,12 +17,37 @@ interface SocialButtonsProps {
 }
 
 const SocialButton: React.FC<SocialButtonsProps> = ({ actionText = "Sign in with" }) => {
+
+      const router=useRouter()
+
+const handleSignIn=async(providerId:ProviderId)=>{
+  try{
+    const result=await signIn(providerId,{
+      redirect:false,
+      callbackUrl:"/" // after login
+    })
+    if(result?.error){
+              alert(`Failed to ${actionText.toLowerCase()}`);
+      console.log(result.error)
+    }else{
+      console.log(`${actionText} successful!`)
+              alert(`${actionText} successful! Redirecting to home...`);
+          router.push('/')
+    }
+  }
+  catch(error){
+   alert("An unexpected error occurred.");
+      console.error("Social sign-in error:", error);
+  }
+}
+
   return (
     <div className="flex flex-col gap-3 mt-2">
       {providers.map(({ id, name, color, Icon }) => (
         <button
           key={id}
-          onClick={() => signIn(id)}
+                    onClick={() => handleSignIn(id)}
+          // onClick={() => signIn(id)}
           className={`${color} text-white py-2 rounded-md font-semibold flex items-center justify-center gap-2`}
           aria-label={`${actionText} ${name}`}
           type="button"
